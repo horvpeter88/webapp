da2b46f16132d01992b820212032b8ddc04dba2f
Peter Horvath <horv.peter88@gmail.com>
2017-11-12 17:34:00 +0100

    Bootstrap hozzáadva, termékek modell kész

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,5 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", price: x*100, image: "placeholder"})
+	x = x + 1
 end
